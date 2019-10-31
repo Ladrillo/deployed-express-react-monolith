a7bc3f172517ddcb81d986d278408cf8372431ae
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-31 12:13:50 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    axios.get('/api/friends')
+    axios.get((process.env.REACT_APP_API || '') + '/api/friends')
       .then(res => {
         setData(res.data);
       })
